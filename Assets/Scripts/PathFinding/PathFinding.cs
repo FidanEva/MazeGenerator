a13b5838b302e7962a96f9bc92ee15c92726564e
FidanEva <86987094+FidanEva@users.noreply.github.com>
2024-11-04 18:48:38 +0400

    Reapply "path node"
    This reverts commit d34c9a7cc12b9581934322d48986b7589f555482.

---
+++ Assets/Scripts/PathFinding/PathFinding.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace PathFinding
+{
+    public class PathFinding
+    {
+        private const int MOVE_STRAIHGT_COST = 10;
+        private const int MOVE_DIAGONAL_COST = 10;
+
+        private List<PathNode> openList;
+        private List<PathNode> closedList;
+    }
+}
---
+++ Assets/Scripts/PathFinding/PathNode.cs
@@ -0,0 +1,19 @@
+﻿namespace PathFinding
+{
+    public class PathNode
+    {
+        public int x;
+        public int y;
+        
+        public int gCost;
+        public int hCost;
+        public int fCost;
+
+        public PathNode cameFromNode;
+
+        public void CalculateFCost()
+        {
+            fCost = gCost + hCost;
+        }
+    }
+}
